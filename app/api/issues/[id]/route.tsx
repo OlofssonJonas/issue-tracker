939b0450771ab385be3706dce5e4385295f2c327
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2023-11-07 11:28:03 +0100

    Adding API for deleting an issue

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -23,9 +23,23 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
             description: body.description
         }
     })
-
     return NextResponse.json(updatedIssue)
 }
 
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string}}) {
+
+     const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id)}, 
+     })
+
+     if(!issue)
+     return NextResponse.json('Issue does not exist', { status: 404 })
+
+     const deletedIssue = await prisma.issue.delete({
+        where: {id: issue?.id}
+     })
+     return NextResponse.json(`Issue with id: ${issue.id} was successfully deleted`)
+}
 
 
