29ad44fd275d2d4bdf63bd0983ef90db07447716
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2023-11-11 16:14:45 +0100

    Change the layout of the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,7 +1,7 @@
 
 'use client'
 
-import { Box } from '@radix-ui/themes'
+import { Box, Container, Flex } from '@radix-ui/themes'
 import classNames from 'classnames'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
@@ -18,23 +18,29 @@ const Navbar = () => {
     const { status, data: session} = useSession()
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+    <nav className=' border-b mb-5 px-5 h-14 py-3'>
+      <Container>
+      <Flex justify='between'>
+        <Flex align='center' gap='3'>
         <Link href="/"><AiFillBug /></Link>
         <ul className='flex space-x-6'>
             {links.map(link => 
             <li key={link.href}>
                 <Link 
                className={classNames({
-                'text-zinc-900': link.href === currentPath,
-                'text-zinc-500': link.href !== currentPath,
-                'hover:text-zinc-800 transition-colors': true
-               })}
+                 'text-zinc-900': link.href === currentPath,
+                 'text-zinc-500': link.href !== currentPath,
+                 'hover:text-zinc-800 transition-colors': true
+                })}
                 href={link.href}>{link.label}</Link></li>)}
         </ul>
+              </Flex>
         <Box>
           { status === "authenticated" && <Link href="/api/auth/signout">Log out</Link>}
           { status === "unauthenticated" && <Link href="/api/auth/signin">Log in</Link> }
         </Box>
+        </Flex>
+        </Container>
     </nav>
   )
 }
