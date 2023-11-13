94baf3d7968a354aa6aa27d4aa7dc88d69a80948
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2023-11-13 12:47:04 +0100

    Assign a issue to a user

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,15 +1,14 @@
 import authOptions from "@/app/auth/authOptions";
 import { issueSchema, patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
-import delay from "delay";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string}})  {
-    const session = await getServerSession(authOptions)
-    if(!session) 
-    return NextResponse.json({}, { status: 401 })
+    // const session = await getServerSession(authOptions)
+    // if(!session) 
+    // return NextResponse.json({}, { status: 401 })
 
     const body = await request.json()
     const validation = patchIssueSchema.safeParse(body)
@@ -18,7 +17,7 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
     
     const { assignedToUserId, title, description } = body 
     if(assignedToUserId) {
-        const user = await prisma.issue.findUnique({
+        const user = await prisma.user.findUnique({
             where: { id: assignedToUserId }
         })
         if(!user)
@@ -38,7 +37,7 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
             title,
             description,
             assignedToUserId
-        }
+        },
     })
     return NextResponse.json(updatedIssue)
 }
--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -6,8 +6,9 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Skeleton }from '@/app/components'
 import toast, { Toaster } from 'react-hot-toast'
+import { Issue } from '@prisma/client'
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue}) => {
   const { data:users, error, isLoading } = useQuery<User[]>({
     queryKey: ['users'],
     queryFn: () => axios.get('/api/users').then(res => res.data),
@@ -20,11 +21,16 @@ const AssigneeSelect = () => {
   if(error) return null
 
   return (
-    <Select.Root>
+    <Select.Root 
+    defaultValue={issue.assignedToUserId || ""}
+    onValueChange={(userId) => {
+      axios.patch('/api/issues/' + issue.id, { assignedToUserId: userId})
+    }}>
       <Select.Trigger placeholder='Assign...' />
           <Select.Content>
               <Select.Group>
                   <Select.Label>Suggestions</Select.Label>
+                  <Select.Item value={null}>Unassigned</Select.Item>
                   { users?.map(user => (
                   <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
                   ))}
